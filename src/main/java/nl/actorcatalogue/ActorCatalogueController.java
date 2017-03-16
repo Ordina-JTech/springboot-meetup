1e92fa425e33fcbb335c1df3264af3cc0d7ea40e
Michel Schudel <michel.schudel@gmail.com>
2017-03-16 11:05:10 +0100

    Step 4: Inject property values

--- src/main/java/nl/actorcatalogue/ActorCatalogueController.java
+++ src/main/java/nl/actorcatalogue/ActorCatalogueController.java
@@ -1,14 +1,18 @@
 package nl.actorcatalogue;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ActorCatalogueController {
 
+    @Value("${app.greeting}")
+    private String greeting;
+
     @RequestMapping("/greeting")
     public String greeting() {
-        return "Hello, world!";
+        return greeting;
     }
 
     @RequestMapping("/actor")
