5b7d33a63526d7f64a6819b3210c5740b4fc1d84
Michel Schudel <michel.schudel@gmail.com>
2017-03-16 11:27:55 +0100

    Step 9: Introduce caching

--- src/main/java/nl/actorcatalogue/ActorRepository.java
+++ src/main/java/nl/actorcatalogue/ActorRepository.java
@@ -6,6 +6,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,12 +17,14 @@ public class ActorRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Cacheable("actors")
     public List<Actor> getAllActors() {
         Query query = entityManager.createQuery("SELECT a FROM Actor a");
         return (List<Actor>) query.getResultList();
     }
 
     @Transactional
+    @CacheEvict(value = "actors", allEntries = true)
     public void storeReceipe(final Actor actor) {
         entityManager.persist(actor);
     }
--- src/main/java/nl/actorcatalogue/MyApplication.java
+++ src/main/java/nl/actorcatalogue/MyApplication.java
@@ -3,9 +3,11 @@ package nl.actorcatalogue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppConfig.class)
+@EnableCaching
 public class MyApplication {
 
     public static void main(String args[]) {
