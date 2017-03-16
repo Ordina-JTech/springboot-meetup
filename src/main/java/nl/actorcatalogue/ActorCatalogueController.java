4f31e16b4682580d5ee5e9dc3e18d3fda81e360f
Michel Schudel <michel.schudel@gmail.com>
2017-03-16 10:58:12 +0100

    Step 2: First simple controller

---
+++ src/main/java/nl/actorcatalogue/ActorCatalogueController.java
@@ -0,0 +1,13 @@
+package nl.actorcatalogue;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ActorCatalogueController {
+
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return "Hello, world!";
+    }
+}
